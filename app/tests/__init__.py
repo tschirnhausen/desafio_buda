eea2d9fed2f9760bdc5e0f82b94b6c52ee037830
Javier Valenzuela <javier.valenzuelac@mail.udp.cl>
2022-11-14 10:55:29 -0300

    fix: change imports for isolated tests usage

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -1,1 +1,3 @@
-import buda
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
--- app/tests/test_live_sdk_buda.py
+++ app/tests/test_live_sdk_buda.py
@@ -1,4 +1,5 @@
 import pytest
+
 from buda import buda, exceptions
 from typing import Optional
 
